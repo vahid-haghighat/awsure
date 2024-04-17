40eb5c059f1ddf4a1c08395f0425c43eca2183ee
Vahid Haghighat <work@haghighat.com>
2024-04-17 15:36:18 -0600

    feat: Added config command

---
+++ cmd/configure.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Configures the cli",
+	Long:  `Configures the cli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(profile)
+	},
+}
+var configCmd = configureCmd
+
+func init() {
+	configCmd.Use = "config"
+	rootCmd.AddCommand(configureCmd)
+	rootCmd.AddCommand(configCmd)
+}
