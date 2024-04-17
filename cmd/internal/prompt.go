dcea9d070b30c7293ac39d0a86a67428a2346d6a
Vahid Haghighat <work@haghighat.com>
2024-04-17 15:49:03 -0600

    feat: Added prompt

---
+++ cmd/internal/prompt.go
@@ -0,0 +1,53 @@
+package internal
+
+import (
+	"github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/manifoldco/promptui"
+	"strings"
+)
+
+type Prompt interface {
+	Select(label string, toSelect []string, searcher func(input string, index int) bool) (index int, value string)
+	Prompt(label string, dfault string) string
+}
+
+type Prompter struct {
+}
+
+func (receiver *Prompter) Select(label string, toSelect []string, searcher func(input string, index int) bool) (int, string, error) {
+	prompt := promptui.Select{
+		Label:             label,
+		Items:             toSelect,
+		Size:              20,
+		Searcher:          searcher,
+		StartInSearchMode: true,
+	}
+	return prompt.Run()
+}
+
+func (receiver *Prompter) Prompt(label string, defaultValue string) (string, error) {
+	prompt := promptui.Prompt{
+		Label:     label,
+		Default:   defaultValue,
+		AllowEdit: false,
+	}
+	return prompt.Run()
+}
+
+func fuzzySearchWithPrefixAnchor(itemsToSelect []string, linePrefix string) func(input string, index int) bool {
+	return func(input string, index int) bool {
+		role := itemsToSelect[index]
+
+		if strings.HasPrefix(input, linePrefix) {
+			if strings.HasPrefix(role, input) {
+				return true
+			}
+			return false
+		} else {
+			if fuzzy.MatchFold(input, role) {
+				return true
+			}
+		}
+		return false
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module awsure
 go 1.22
 
 require (
+	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/manifoldco/promptui v0.9.0
+	github.com/spf13/cobra v1.8.0
+)
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
