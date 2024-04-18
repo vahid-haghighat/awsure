3920ec8559f0ac7371a22c96efb0f66d2b41e03e
Vahid Haghighat <work@haghighat.com>
2024-04-18 12:56:49 -0600

    feat: Added the config remove command to git.

---
+++ cmd/configRemove.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"awsure/cmd/internal"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+// deleteCmd represents the delete command
+var configRemoveCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Removes a configuration profile",
+	Long:  `Removes a configuration profile`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profile, err := cmd.Flags().GetString("profile")
+		if err != nil {
+			return err
+		}
+
+		return internal.ConfigRemove(profile)
+	},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if !cmd.Flags().Changed("profile") {
+			return fmt.Errorf("the profile name to remove should explicitly be specified")
+		}
+		return nil
+	},
+}
+
+func init() {
+	configCmd.AddCommand(configRemoveCmd)
+}
