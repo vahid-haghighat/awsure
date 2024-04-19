99658b14b34cdbb5bad3cc6e63e086eebfba7acc
Vahid Haghighat <work@haghighat.com>
2024-04-18 21:00:37 -0600

    fix: Removed unnecessary flags.

--- cmd/root.go
+++ cmd/root.go
@@ -32,8 +32,4 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&configuration.Profile, "profile", "p", "default", "The name of the profile to log in with or configure (This is mutually exclusive with --all-profiles)")
 	rootCmd.PersistentFlags().BoolVarP(&configuration.AllProfiles, "all-profiles", "a", false, "Log in or configure all profiles (This is mutually exclusive with --profile)")
-	rootCmd.Flags().BoolVarP(&configuration.ForceRefresh, "force-refresh", "f", false, "Force refresh of the access tokens")
-	rootCmd.Flags().StringVarP(&configuration.Mode, "mode", "m", "cli", "'cli' to hide the login page and perform the login through the CLI, 'gui' to perform the login through the Azure GUI (more reliable but only works on GUI operating system and also the default behaviour)")
-	rootCmd.Flags().BoolVarP(&configuration.NoVerifySSL, "no-verify-ssl", "s", false, "Disable SSL certificate verification")
-	rootCmd.Flags().BoolVarP(&configuration.NoPrompt, "no-prompt", "t", false, "Disable prompting and use default configuration values")
 }
--- cmd/types/configuration.go
+++ cmd/types/configuration.go
@@ -1,10 +1,6 @@
 package types
 
 type Configuration struct {
-	Profile      string
-	AllProfiles  bool
-	ForceRefresh bool
-	Mode         string
-	NoVerifySSL  bool
-	NoPrompt     bool
+	Profile     string
+	AllProfiles bool
 }
